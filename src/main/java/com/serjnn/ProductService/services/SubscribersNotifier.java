dc1c293e4e964b8b295e80ec257255619a0b6cda
sergey <sersh200606@gmail.com>
2026-04-27 21:45:41 +0300

    ref: logging ref to notifier

--- src/main/java/com/serjnn/ProductService/services/SubscribersNotifier.java
+++ src/main/java/com/serjnn/ProductService/services/SubscribersNotifier.java
@@ -12,6 +12,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 @Slf4j
@@ -23,25 +25,32 @@ public class SubscribersNotifier {
     private String discountNotifChannel;
 
     public void notifySubscribers(DiscountChangesDto discountChangesDto) {
-        log.info("notifying subscribers " + discountChangesDto);
+        log.info("Starting notification process for product {}. Data: {}", discountChangesDto.productId(), discountChangesDto);
         Long productId = discountChangesDto.productId();
         
         int pageSize = 100;
         Pageable pageable = PageRequest.of(0, pageSize);
         Slice<Long> clientIdsSlice;
+        int totalNotified = 0;
         
         do {
             clientIdsSlice = subscribersRepository.findClientIdsByProductId(productId, pageable);
-            clientIdsSlice.getContent().forEach(clientId -> {
+            List<Long> clientIds = clientIdsSlice.getContent();
+            log.info("Found {} subscribers for product {} in current slice (page: {})", clientIds.size(), productId, pageable.getPageNumber());
+            
+            clientIds.forEach(clientId -> {
                 DiscountNotification notification = new DiscountNotification(
                         discountChangesDto.productId(),
                         clientId,
                         discountChangesDto.newDiscount()
                 );
-                log.info("NOTIFYING SUBSCRUBER {}", notification.clientId());
+                log.info("NOTIFYING SUBSCRIBER {} for product {}", notification.clientId(), notification.productId());
                 redisTemplate.convertAndSend(discountNotifChannel, notification);
             });
+            totalNotified += clientIds.size();
             pageable = pageable.next();
         } while (clientIdsSlice.hasNext());
+        
+        log.info("Notification process finished. Total subscribers notified: {}", totalNotified);
     }
 }
